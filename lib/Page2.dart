8fdbd59c878578d556f8b21658724928e27e103b
im_aldi <aldiirsanmajid@gmail.com>
2020-11-10 11:27:48 +0700

    commit 2

--- lib/Page2.dart
+++ lib/Page2.dart
@@ -1,116 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'page2.dart';
+import 'main.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Page2());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Page2 extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Covid 19 News',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Covid 19 News'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-
-}
-
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-          title: Center(
-            child: Text(widget.title),
-          )
+        title: Center(
+          child: Text('Covid 19 News'),
+        ),
       ),
       body: Center(
         child: ListView(
-            padding: EdgeInsets.only(top: 150.0),
+            padding: EdgeInsets.only(top: 50.0),
             children: <Widget>[
               Container (
                   child: Center(
-                      child: Text('All Cases : 272691',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 22.0,
-                          )
-                      )
-                  )
-              ),
-              Container(
-                  child: Center(
-                      child: Text('All Deaths : 11310',
+                      child: Text('INDIA',
                           style: TextStyle(
                             color: Colors.red,
-                            fontSize: 22.0,
+                            fontSize: 40.0,
                           )
                       )
                   )
               ),
               Container(
                   child: Center(
-                      child: Text('All Recovered : 90618',
+                      child: Text('Cases : 249',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
+                          )
+                      )
+                  )
+              ),
+              Container(
+                  child: Center(
+                      child: Text('Today Cases : 55',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 18.0,
+                          )
+                      )
+                  )
+              ),
+              Container(
+                  child: Center(
+                      child: Text('Deaths : 5',
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 18.0,
+                          )
+                      )
+                  )
+              ),
+              Container(
+                  child: Center(
+                      child: Text('Today Deaths : 1',
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 18.0,
+                          )
+                      )
+                  )
+              ),
+              Container(
+                  child: Center(
+                      child: Text('Recovered : 23',
                           style: TextStyle(
                             color: Colors.green,
-                            fontSize: 22.0,
+                            fontSize: 18.0,
                           )
                       )
                   )
               ),
               Container(
                   child: Center(
-                      child: Text('All Active Cases : 170763',
+                      child: Text('Active Cases : 221',
                           style: TextStyle(
                             color: Colors.yellow,
-                            fontSize: 22.0,
+                            fontSize: 18.0,
+                          )
+                      )
+                  )
+              ),
+              Container(
+                  child: Center(
+                      child: Text('Critical : 0',
+                          style: TextStyle(
+                            color: Colors.yellow,
+                            fontSize: 18.0,
+                          )
+                      )
+                  )
+              ),
+              Container(
+                  child: Center(
+                      child: Text('Cases Per Million : 0',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18.0,
                           )
                       )
                   )
@@ -193,18 +187,17 @@ class _MyHomePageState extends State<MyHomePage> {
                   width: 100.0,
                   child: RaisedButton(
                     color: Colors.yellow,
-                    child: Text('Next Page'),
+                    child: Text('Back'),
                     onPressed: (){
                       Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (context) => Page2())
+                          MaterialPageRoute(builder: (context) => MyApp())
                       );
                     },
                   )
               )
             ]
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      ),
+    ); // This trailin
   }
 }
-
